c5a589f0f5351e37cbd84e404d78cb1d7ac694b1
Dave Judge <143704883+lvl7wizard@users.noreply.github.com>
2024-06-05 13:47:39 +0100

    creates prisma settings to ensure only one instance of prisma runs at one time

---
+++ lib/prisma.ts
@@ -0,0 +1,11 @@
+// This code ensures that only one instance of PrismaClient is created and used throughout the application
+
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
